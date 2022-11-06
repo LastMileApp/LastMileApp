e4b2985808f9723ea502c7b3fcf9dca3b3c6e160
Ari Kamat <arijkamat@gmail.com>
2022-11-05 22:13:25 -0400

    map shows general region of location

--- components/Where.js
+++ components/Where.js
@@ -1,135 +1,179 @@
 import * as React from 'react';
-import { Pressable, View, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native';
+import { Pressable, View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { useState, useEffect } from 'react';
-import {getLatLong} from '../services/maps.js'
+import { getLatLong } from '../services/maps.js'
 import * as Location from 'expo-location'
+import MapView from 'react-native-maps';
 
-function Where ({ navigation })  {
-    const [location, setLocation] = useState('');
-    const [endLocation, setEndLocation] = useState('');
-    const [endLocLatLon, setEndLocLatLon] = useState('');
-    async function actionsOnPress(endLocation){
-      let a = await getLatLong(endLocation);
-      console.log("aasdfadsf");
-      console.log(a);
 
-      navigation.navigate('Packages', { currentLocation: location, endLocation:endLocation});
-    }
-    useEffect(() => {
-        (async () => {
-            let { status } = await Location.requestForegroundPermissionsAsync();
-            if (status !== 'granted') {
-                return;
-            }
 
-            let location = await Location.getCurrentPositionAsync({
-                accuracy: Location.Accuracy.Balanced,
-                enableHighAccuracy: true,
-                timeInterval: 5
-            });
-            console.log(location);
-            setLocation(location);
-        })();
-    }, []);
 
-    // useEffect(() => {
-    //     (async () => {
-    //         let endLocLatLon = await getLatLong(endLocation);
-    //         // setEndLocLatLon(endLocLatLon);
-    //     })();
-    // });
-        
-  return (
-    <View style={styles.container}>
-        <Text style={styles.title}>Where are you going?</Text>
-      <View style={styles.input}>
-        
-        <TextInput
-          style = {styles.TextIn}
-          placeholder = "Full Address"
-          onChangeText={(endLocation) => setEndLocation(endLocation)} />
-        
-      </View>
 
-      {/* <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      /> */}
-      {/* <Pressable style={styles.button} onPress={onPress}></Pressable> */}
-      <Pressable style={styles.button} onPress={() => actionsOnPress(endLocation)}  >
-        <Text style={styles.text}>Go</Text>
-        </Pressable>
-    </View>
-  );}
+function Where({ navigation }) {
+	const [location, setLocation] = useState('');
+	const [mapRegion, setMapRegion] = useState({
+		latitude: 0,
+		longitude: -122.4324,
+		latitudeDelta: 0.0922,
+		longitudeDelta: 0.0421,
+	});
+	const [endLocation, setEndLocation] = useState('');
+	const [endLocLatLon, setEndLocLatLon] = useState('');
+	async function actionsOnPress(endLocation) {
+		let a = await getLatLong(endLocation);
+		console.log("aasdfadsf");
+		console.log(a);
+
+		navigation.navigate('Packages', { currentLocation: location, endLocation: endLocation });
+	}
+	useEffect(() => {
+		(async () => {
+			let { status } = await Location.requestForegroundPermissionsAsync();
+			if (status !== 'granted') {
+				return;
+			}
+
+			let location = await Location.getCurrentPositionAsync({
+				accuracy: Location.Accuracy.Balanced,
+				enableHighAccuracy: true,
+				timeInterval: 5
+			});
+			console.log(location);
+			setLocation(location);
+			a={latitude:location.coords.latitude,longitude:location.coords.longitude,latitudeDelta: 0.0922,
+			longitudeDelta: 0.0421,};
+			setMapRegion(a);
+		})();
+	}, []);
+
+	// useEffect(() => {
+	//     (async () => {
+	//         let endLocLatLon = await getLatLong(endLocation);
+	//         // setEndLocLatLon(endLocLatLon);
+	//     })();
+	// });
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Where are you going?</Text>
+			<View style={styles.input}>
+
+				<TextInput
+					style={styles.TextIn}
+					placeholder="Full Address"
+					onChangeText={(endLocation) => setEndLocation(endLocation)} />
+
+			</View>
+			<View style={styles.containerMapAndButton}>
+				<View style={styles.containerMap}>
+					<MapView
+						style={{
+							alignSelf: 'stretch',
+							height: '100%',
+							flex:1
+						}}
+					region = {mapRegion}
+					/>
+				</View>
+				<View style={styles.containerButton}>
+					<Pressable style={styles.button} onPress={() => actionsOnPress(endLocation)}  >
+						<Text style={styles.text}>Go</Text>
+					</Pressable>
+				</View>
+			</View>
+		</View>
+	);
+}
 
 export default Where;
 
 
 const styles = StyleSheet.create({
-    title: {
-        height: 90,
-        fontSize:35,
-        padding: 5,
-    },
-    container: {
-      flex: 1,
-      backgroundColor: "#86CB92",
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  
-    image:{
-      marginBottom: 50
-    },
-  
-    input: {
-      backgroundColor: '#71b48d',
-      borderRadius: 30,
-      width: "80%",
-      height: 80,
-      marginBottom: 400,
-      alignItems: "center",
-    },
-    
-    TextIn: {
-      height: 50,
-      flex: 1,
-      padding: 30,
-      marginLeft: 0,
-    },
-  
-    forgot_button: {
-      height: 30,
-      marginBottom: 30,
-    },
-   
-    loginButton: {
-      width: "80%",
-      borderRadius: 55,
-      height: 50,
-      alignItems: "center",
-      justifyContent: "center",
-      marginTop: 40,
-      backgroundColor: "#6359C",
-    },
-    button: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 12,
-        paddingHorizontal: 32,
-        borderRadius: 4,
-        elevation: 3,
-        backgroundColor: '#71b48d',
-      },
-      text: {
-        fontSize: 16,
-        lineHeight: 21,
-        fontWeight: 'bold',
-        letterSpacing: 0.25,
-        color: 'black',
-      },
-  });
-  
+	title: {
+		height: 90,
+		fontSize: 35,
+		padding: 5,
+		marginTop:'15%'
+	},
+	container: {
+		flex: 1,
+		backgroundColor: "#86CB92",
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	containerMapAndButton: {
+		flex: 1,
+		marginBottom: 0,
+		marginLeft: 0,
+		marginRight: 0,
+		width: '100%',
+		alignItems: 'center',
+	},
+	containerMap: {
+		flex: 1,
+		width: '80%',
+		top: '20%',
+		marginBottom:'30%',
+		bottom: '20%',
+		height: '10%',
+	},
+	containerButton: {
+		position: 'absolute',
+		marginBottom:10
+	},
+	image: {
+		marginBottom: 50
+	},
+
+	input: {
+		backgroundColor: '#71b48d',
+		borderRadius: 30,
+		width: "80%",
+		height: 80,
+		marginTop: '20%',
+		marginBottom:'10%',
+		alignItems: "center",
+	},
+
+	TextIn: {
+		height: 50,
+		flex: 1,
+		padding: 30,
+		marginLeft: 0,
+	},
+
+	forgot_button: {
+		height: 30,
+		marginBottom: 30,
+	},
+
+	loginButton: {
+		width: "80%",
+		borderRadius: 55,
+		height: 50,
+		alignItems: "center",
+		justifyContent: "center",
+		marginTop: 40,
+		backgroundColor: "#6359C",
+	},
+	button: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		paddingVertical: 12,
+		paddingHorizontal: 32,
+		borderRadius: 4,
+		elevation: 3,
+		backgroundColor: '#71b48d',
+	},
+	text: {
+		fontSize: 16,
+		lineHeight: 21,
+		fontWeight: 'bold',
+		letterSpacing: 0.25,
+		color: 'black',
+	},
+});
+
